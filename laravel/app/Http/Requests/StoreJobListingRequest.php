a4af7286faae101399d4977de30e52e17ef02081
Rachel Fish <itsrachelfish@gmail.com>
2026-05-17 03:02:48 -0600

    Request validation boilerplate for job listings

---
+++ laravel/app/Http/Requests/StoreJobListingRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreJobListingRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return JobListingRules::rules();
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge(JobListingRules::normalize($this->all()));
+    }
+}
